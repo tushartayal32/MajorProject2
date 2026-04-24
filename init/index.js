41c3fd8b3d8b131156c6cfd0d2759b2a54e3e4f0
Tushar <tushartayal32@gmail.com>
2026-04-24 11:55:09 +0530

    fix: add geometry and DB init

--- init/index.js
+++ init/index.js
@@ -1,26 +1,33 @@
+require("dotenv").config({
+  path: require("path").resolve(__dirname, "../.env"),
+});
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.ATLASDB_URL;
 
-main()
-  .then(() => {
-    console.log("connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+console.log("URL:", process.env.ATLASDB_URL);
 
-async function main() { 
+async function main() {
   await mongoose.connect(MONGO_URL);
+  console.log("connected to DB");
+  await initDB();
 }
+
+main().catch((err) => console.log(err));
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  initData.data = initData.data.map((obj) => ({...obj, owner: "66f7f6554a00e701aa41e0c9"}));
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "66f7f6554a00e701aa41e0c9",
+    geometry: {
+      type: "Point",
+      coordinates: [77.209, 28.6139], // dummy (Delhi)
+    },
+  }));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
-
-initDB();
